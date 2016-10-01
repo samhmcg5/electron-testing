64c05403bc31b6567469f88f9f1e6012d6f3569b
Sam McGhee <samhmcg5@vt.edu>
2016-10-01 14:14:19 -0400

    refactor

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { ParentModule } from './parent.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(ParentModule);
---
+++ src/app/parent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  template:
+  `
+  <my-app>Loading My App</my-app>
+  `
+})
+
+export class ParentComponent{}
